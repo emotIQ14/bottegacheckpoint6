0b2cd085500f0c63a70b39f86019bfe81026526f
emotIQ14 <anderbilbaocastejon@gmail.com>
2026-03-09 22:21:22 +0100

    Add Usuario exercise and Colab link

---
+++ README.md
@@ -0,0 +1,5 @@
+# bottegacheckpoint6
+
+## Ejercicios solicitados
+
+- Colab: [Parte de ejercicios](https://colab.research.google.com/drive/1XfnrAgZdw4bRMHw2KtockQM_qJq0ZnkL?usp=sharing)
---
+++ usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, nombre_usuario, contrasena):
+        self.nombre_usuario = nombre_usuario
+        self.contrasena = contrasena
+
+
+usuario1 = Usuario("ander", "clave123")
